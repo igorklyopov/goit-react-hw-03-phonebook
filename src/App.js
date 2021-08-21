25f27ca118c7bd780542d0c9329bdae5f4c5488f
Igor Klyopov <shoot8202@gmail.com>
2021-08-21 18:18:26 +0300

    adds functionality for saving contacts to localStorage

--- src/App.js
+++ src/App.js
@@ -8,16 +8,29 @@ import Section from "./components/Section/Section";
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
   };
 
-  static propTypes = {};
+  componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+
+    if (savedContacts) {
+      this.setState({ contacts: savedContacts });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+
+    const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+
+    if (savedContacts.length === 0) {
+      localStorage.removeItem("contacts");
+    }
+  }
 
   addContact = (name, number) => {
     const contactsItem = {
